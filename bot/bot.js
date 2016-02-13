1a72e1845d6d15ee97b04c678c57ac32b4a99d44
Saeed Dadkhah <sa.dadkhah@gmail.com>
2016-02-13 19:00:28 +0330

    tried to add custom button to bot

--- bot/bot.js
+++ bot/bot.js
@@ -3,10 +3,15 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '195454726:AAGNNWm6vzcHEZrdjH5KfYtjhNanIbB96Ug'
 
 var bot = new TelegramBot(token, {polling:true});
-
+var opts = {
+   reply_markup: JSON.stringify(
+                       {
+                          keyboard:[["ایرانسل", "bbbb"]]
+                       }
+                       )};
 bot.on('message', function(msg)
       {
          var chat_id = msg.chat.id;
-         bot.sendMessage(chat_id, '/1. salam');
+         bot.sendMessage(chat_id, '/salam', opts);
       });
 
