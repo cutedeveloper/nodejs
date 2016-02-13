c1414b1dcbdc4d2777f3dbc178521d8d5b8780cc
Saeed Dadkhah <sa.dadkhah@gmail.com>
2016-02-13 23:33:32 +0330

    ordinary commit'

--- bot/bot.js
+++ bot/bot.js
@@ -2,16 +2,23 @@ var TelegramBot = require('node-telegram-bot-api');
 
 var token = '195454726:AAGNNWm6vzcHEZrdjH5KfYtjhNanIbB96Ug'
 
+function User(chat_id)
+{
+   this.chat_id = chat_id;
+}
+
+User.prototype.state = "initiated";
+
 var bot = new TelegramBot(token, {polling:true});
 var opts = {
    reply_markup: JSON.stringify(
                        {
-                          keyboard:[["ایرانسل", "bbbb"]]
+                          keyboard:[["ایرانسل", "همراه اول"], ["رایتل"]]
                        }
                        )};
 bot.on('message', function(msg)
       {
          var chat_id = msg.chat.id;
-         bot.sendMessage(chat_id, '/salam', opts);
+         bot.sendMessage(chat_id, 'اپراتورت چیه؟ :)', opts);
       });
 
