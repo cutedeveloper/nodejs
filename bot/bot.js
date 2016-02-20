f25c93b9e960ba5e449d90c4264328a7582e7539
Saeed Dadkhah <sa.dadkhah@gmail.com>
2016-02-20 18:59:55 +0330

    openshift compatibility added

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,16 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+     console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
+
 var TelegramBot = require('node-telegram-bot-api');
+var HashMap = require('hashmap');
 
 var token = '195454726:AAGNNWm6vzcHEZrdjH5KfYtjhNanIbB96Ug'
+
+var user_map = new HashMap();
 
 function User(chat_id)
 {
@@ -19,6 +29,15 @@ var opts = {
 bot.on('message', function(msg)
       {
          var chat_id = msg.chat.id;
-         bot.sendMessage(chat_id, 'اپراتورت چیه؟ :)', opts);
+         if (user_map.has(chat_id))
+         {
+            console.log("user is added before");
+            console.log("current state " + user_map.get(chat_id).state);
+         }
+         else
+      {
+         user_map.set(chat_id, new User(chat_id));
+         bot.sendMessage(chat_id, 'اپراتورت چیه؟ :)', opts); 
+      }
       });
 
